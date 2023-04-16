44d4eb4504683bddc3577f9227e1038d1c9c0b7b
clebson-marcos <93105705+clebson-marcos@users.noreply.github.com>
2023-04-16 13:03:18 -0400

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,27 @@
 import datetime
 
-
-class systemInfo:
-    def __init__()
-    pass
+# A class to return system info.
+class SystemInfo:
+    def __init__(self):
+        pass
     
     @staticmethod
     def get_time():
         now = datetime.datetime.now()
-        answer = 'Saão {} horas e {} minutos.'.format(now.hour, now.minute)
+        answer = 'The time now is {} {}'.format(now.hour, now.minute)
         return answer
+
+'''
+    @staticmethod
+    def get_time():
+        now = datetime.datetime.now()
+        answer = 'The date is {} {}'.format(now.hour, now.minute)
+        return answer
+
+
+    @staticmethod
+    def get_year():
+        now = datetime.datetime.now()
+        answer = 'The current year is {}'.format(now.hour, now.minute)
+        return answer
+'''
